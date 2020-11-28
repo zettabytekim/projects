3c0f817a3f0973959eca0d423bf1022d9893603a
Zetta Kim <zettabyte.kim@gmal.com>
2020-11-28 21:37:07 +0900

    11/28 - 1 commit

--- myproject/pybo/__init__.py
+++ myproject/pybo/__init__.py
@@ -9,15 +9,16 @@ migrate = Migrate()
 
 
 def create_app():
-		app = Flask(__name__)
-		app.config.from_object(config)
+    app = Flask(__name__)
+    app.config.from_object(config)
 
-		# ORM
-		db.init_app(app)
-		migrate.init_app(app, db)
+    # ORM
+    db.init_app(app)
+    migrate.init_app(app, db)
+    from . import models
 
-		# 블루프린트
-		from .views import main_views
-		app.register_blueprint(main_views.bp)
+    # 블루프린트
+    from .views import main_views
+    app.register_blueprint(main_views.bp)
 
-		return app
+    return app
